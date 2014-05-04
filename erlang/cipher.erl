4f2e6221d0b9ee2d6e31972a5ad819c2496ae135
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-05-04 13:45:57 -0400

    completed - erlang was fun, lisp is still horrible but works

--- erlang/cipher.erl
+++ erlang/cipher.erl
@@ -1,9 +1,53 @@
-InputString = "JOHN PAUL WELSH".
-ShiftAmt = 3.
-MaxShift = 26.
-    
-shift({Num, ShiftAmt}) ->
-    Num + ShiftAmt.
+% Caesar Cipher - Erlang
+% Author: John Paul Welsh
+-module(cipher).
+-export([shift/2]).
+-export([encrypt/2]).
+-export([decrypt/2]).
+-export([solve/2]).
+-export([display/0]).
 
-mapShift({InputList, ShiftAmt}) ->
-    [shift({ShiftAmt, Num}) || Num <- InputList].
+% Shifts a number, with bounds-checking, and ignoring spaces.
+shift(Num, Amt) ->
+	Temp = Num+Amt,
+	if
+		Num =:= 32 -> Num;
+		Temp < 65 -> (91 - (65 rem Temp));
+		Temp > 90 -> (64 + (Temp rem 90));
+		true -> Temp
+	end.
+
+% Shifts the head of the current input, puts that in a list, and
+% concatenates that list with another one containing a recursive
+% call to the function.
+encrypt(Input, Amt) ->
+	if
+		length(Input) < 1 -> [];
+		true -> ([shift(hd(Input), Amt)] ++ [encrypt(tl(Input), Amt)])
+	end.
+
+% Encrypts with a negative number.
+decrypt(Input, Amt) ->
+	encrypt(Input, -Amt).
+
+% Prints the decryption of the current amount, then recursively
+% calls the function with one smaller shift amount, down to 0.
+solve(Input, Max) ->
+	if
+		Max < 0 -> [];
+		true -> io:format("Solve ~w: ~s~n", [Max, decrypt(Input, Max)]),
+			solve(Input, Max-1)
+	end.
+
+% Prints everything to the screen: basically a 'main' function.
+display() ->
+	InputString = "JOHN PAUL WELSH",
+	ShiftAmt = 18,
+	MaxShift = 26,
+
+	io:format("The input string is ~s~n", [InputString]),
+	io:format("The shift amount for encrypt and decrypt is ~w~n", [ShiftAmt]),
+	io:format("The maximum shift amount for solve is ~w~n", [MaxShift]),
+	io:format("Encrypted: ~s~n", [encrypt(InputString, ShiftAmt)]),
+	io:format("Decrypted: ~s~n", [decrypt(InputString, ShiftAmt)]),
+	solve(InputString, MaxShift).
--- lisp/cipher.lisp
+++ lisp/cipher.lisp
@@ -3,16 +3,16 @@
 
 (defparameter *input* "ABCDEFGHIJKLMNOPQRSTUVWXYZ") ;input string
 (defparameter *crypt-shift* 1) ;shift amount for encrypt and decrypt
-(defparameter *solve-shift* 26) ;max shift amount for solve
+(defparameter *solve-shift* 15) ;max shift amount for solve
 
 ;;Shifts the ASCII value, checks whether it exceeds any bounds,
 ;;keeps it the same if not
 ;;Used by encrypt
 (defun shift (n)
-		(if (= n 32) ;if it's a space...
+    (if (= n 32) ;if it's a space...
         (+ n 0) ;don't change it, just return
       (if (< (+ n *crypt-shift*) 65) ;if it's below A...
-      		(- 91 (mod 65 (+ n *crypt-shift*))) ;wrap it around to Z and descend
+          (- 91 (mod 65 (+ n *crypt-shift*))) ;wrap it around to Z and descend
         (if (> (+ n *crypt-shift*) 90) ;if it's above Z...
             (+ 64 (mod (+ n *crypt-shift*) 90)) ;wrap it around to A and ascend
           (+ n *crypt-shift*) ;otherwise, just add the shift and return
@@ -36,17 +36,32 @@
     )
 )
 
+;;Same as shift function, but subtracts the solve shift amount instead
+;;Used by solve
+(defun solves-shift (n)
+    (if (= n 32)
+        (+ n 0)
+      (if (< (- n *solve-shift*) 65)
+          (- 91 (mod 65 (- n *solve-shift*)))
+        (if (> (- n *solve-shift*) 90)
+            (+ 64 (mod (- n *solve-shift*) 90))
+          (- n *solve-shift*)
+        )
+      )
+    )
+)
+
 ;;Converts a string into a list of characters
 ;;Stolen from
 ;;http://stackoverflow.com/questions/5706803
 ;;  /split-string-into-individual-characters
 (defun explode (input)
-	(coerce input 'list)
+  (coerce input 'list)
 )
 
 ;;Converts a list of characters into a string
 (defun implode (input)
-	(coerce input 'string)
+  (coerce input 'string)
 )
 
 ;;Converts a list of characters to ASCII codes
@@ -54,19 +69,19 @@
 ;;http://stackoverflow.com/questions/5706803
 ;;  /split-string-into-individual-characters
 (defun char-to-int (input)
-	(map 'list #'char-code input)
+  (map 'list #'char-code input)
 )
 
 ;;Converts a list of ASCII codes to characters
 (defun int-to-char (input)
-	(map 'list #'code-char input)
+  (map 'list #'code-char input)
 )
 
 ;;Explodes the input string, maps char-to-int to that,
 ;;  maps shift to that, maps shift-bounds to that, maps
 ;;  int-to-char to that, and implodes that
 (defun en-crypt (input-string)
-  	(implode
+    (implode
         (int-to-char
             (map 'list #'shift
                 (char-to-int
@@ -91,11 +106,49 @@
 )
 
 ;;Solves a string for every shift value between solve-shift and 0
-;(defun solve (input-string)   
-    ;map decrypt across the input string, print it, and call
-    ;solve again with (- solve-shift 1)
-;)
+(defun solve (input-string)
+    (if (>= *solve-shift* 0)
+        (inside-solve input-string)
+    )
+)
 
-(print (en-crypt *input*))
-(print (de-crypt *input*))
+;;Lumps together all the printing statements for solve, since otherwise
+;;  it yelled at me for not using a lambda function
+;;Used by solve
+(defun inside-solve (input-string)
+    (print "Solve ")
+    (prin1 *solve-shift*)
+    (prin1 ": ")
+    (prin1 (do-solve input-string))
+    (decf *solve-shift*)
+    (solve input-string)
+)
+
+;;The same as dec-shift, but uses *solve-shift* instead of *crypt-shift*
+;;Used by inside-solve
+(defun do-solve (input-string)
+    (implode
+        (int-to-char
+            (map 'list #'solves-shift
+                (char-to-int
+                    (explode input-string)
+                )
+            )
+        )
+    )
+)
+
+;;;-------------------------------------------------
+
+(print "The input string is ")
+(prin1 *input*)
+(print "The shift amount for encrypt and decrypt is ")
+(prin1 *crypt-shift*)
+(print "The maximum shift value for solve is ")
+(prin1 *solve-shift*)
+
+(print "Encrypted: ")
+(prin1 (en-crypt *input*))
+(print "Decrypted: ")
+(prin1 (de-crypt *input*))
 (solve *input*)
